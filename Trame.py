755f8b5e0d5c75494381c8ad87ab780f76ac5ea1
Groot-me <62052155+groot-me@users.noreply.github.com>
2022-02-25 22:24:26 +0100

    Add ipv6 protocol and function who return an ipv6 with maximal compression

--- Trame.py
+++ Trame.py
@@ -51,6 +51,7 @@ class Trame:
         # EtherType Protocol supported except ipv6
         EtherType = "0x" + Data[24:28]
         # Ox0800 = "(IPv4)"
+        Ox7373 = "0x7373 (Broadcast)"
         Ox0806 = f"{BYellow}ARP"
         Ox86dd = f"{BWhite}IPv6"
 
@@ -69,9 +70,15 @@ class Trame:
             Packet_data = Data[size_of_frame:len(Data)]
             self.ARP_Header(Packet_data)
 
-        elif (EtherType == "0x86dd"):  # Does not yet support IPv6 protocol
-            print(f"{White} | Protocol : {Ox86dd} Not yet Implemented {Reset}", end="")
-            # print(f"Not yet implemented :,(")
+        elif (EtherType == "0x86dd"):
+            # print(f"{White} | Protocol : {Ox86dd}", end="")
+
+            # We transfer the rest of protocol IPV6
+            Packet_data = Data[size_of_frame:len(Data)]
+            self.IPv6_Packet_Header(Packet_data)
+
+        elif (EtherType == "0x7373"): #it's just a broadcast
+            print(f"{White} | Protocol : {Red}{Ox7373}", end="")
 
         else:
             print(f"{White} unknown : {BRed}", EtherType, end="")
@@ -108,6 +115,8 @@ class Trame:
             print(f"{White} | Target_Ip : {BCyan}", Target_Ip, end="")
 
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@ Layer3 HEADER @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+    # @@@@@@@ IPV4 @@@@@@@@@@
     def IPV4_Packet_Header(self, Data):  # Some fields are not useful, but I let them in comment for understand the ipv4 header
         # Version = Data[0:1]
         Size = int(Data[1:2]) * 8  # 20 bytes normally but sometimes it can change, so we use the packet length to determine his size * 8 (1bytes = 2hex char don't forget ;)
@@ -120,7 +129,7 @@ class Trame:
         Ox01 = f"{BCyan}ICMP"
         Ox06 = f"{BBlue}TCP"
         Ox11 = f"{BBlue}UDP"
-        OxFF = f"{White} | Unknown : {BRed}"
+        Unknown  = f"{White} | Unknown : {BRed}"
 
         if (Protocol == "0x01"):
             Protocol = Ox01
@@ -156,7 +165,7 @@ class Trame:
             # Then transfer it the UDP protocol
             self.UDP_Header(Data[Size:len(Data)])
         else:
-            Protocol = OxFF + Protocol
+            Protocol = Unknown  + Protocol
             print(f"{White} | Protocol : ", Protocol, end="")
 
     def ipv4_conversion(self, HexIp):
@@ -167,6 +176,92 @@ class Trame:
             ipv4 += str(int(HexIp[n:n + 2], base=16))
 
         return ipv4
+
+    #@@@@@@@ IPV6 @@@@@@@@@@
+    def IPv6_Packet_Header(self,Data):
+        # first = Data[0:8] Version, traffic class, Flow Label
+        # Payload_length = Data[8:12]
+        Next_Header = "0x" + Data[12:14] #It's the protocol like ipv4
+        Ox3a = f"{BCyan}ICMPV6"
+        Ox06 = f"{BBlue}TCP"
+        Ox11 = f"{BBlue}UDP"
+        Unknown = f"{White} | Unknown : {BRed}"
+
+        # Hop_Limit = Data[14:16]
+        if (Next_Header == "0x3a"):
+            Protocol = Ox3a
+            print(f"{White} | Protocol : ", Protocol, end="")
+
+            self.IP_Source = f"{BPurple}" + self.ipv6_conversion(Data[16:48])
+            self.IP_Dest = f"{BPurple}" + self.ipv6_conversion(Data[48:80])
+
+            # Just a ping so ....
+            print(f"{White} | IpSrc : ", self.IP_Source, end="")
+            print(f"{White} | IpDst : ", self.IP_Dest, end="")
+
+        elif (Next_Header == "0x06"):
+            Protocol = Ox06
+            print(f"{White} | Protocol : ", Protocol, end="")
+
+            self.IP_Source = f"{BPurple}" + self.ipv6_conversion(Data[16:48])
+            self.IP_Dest = f"{BPurple}" + self.ipv6_conversion(Data[48:80])
+
+            # Then transfer it to the TCP protocol
+            self.TCP_Header(Data[80:len(Data)])
+
+        elif (Next_Header == "0x11"):
+            Protocol = Ox11
+            print(f"{White} | Protocol : ", Protocol, end="")
+
+
+            self.IP_Source = f"{BPurple}" + self.ipv6_conversion(Data[16:48])
+            self.IP_Dest = f"{BPurple}" + self.ipv6_conversion(Data[48:80])
+
+            # Then transfer it the UDP protocol
+            self.UDP_Header(Data[80:len(Data)])
+        else:
+            Protocol = Unknown  + Next_Header
+            print(f"{White} | Protocol : ", Protocol, end="")
+
+    def ipv6_conversion(self,HexIp):
+        ipv6 = ""
+        first_zero = False
+        # Remove all useless zero
+        for n in range(0, len(HexIp), 4):
+            check = n
+            if (HexIp[check] == "0"):
+                first_zero = True
+
+            for next in range(n, n + 4, 1):
+
+                if (HexIp[check] == "0" and first_zero):
+                    ipv6 += ""
+                    check += 1
+                else:
+                    ipv6 += HexIp[next]
+
+            ipv6 += ":"
+
+        # Remove all useless semi colon
+        cpt_semi_colon_start = 0
+        cpt_semi_colon_end = 0
+        Not_find = True
+        for i in range(len(ipv6) - 1):
+
+            if (ipv6[i] == ":" and Not_find):
+                if (ipv6[i + 1] == ":"):
+                    cpt_semi_colon_start = i
+                    Not_find = False
+
+            if (Not_find == False and ipv6[i + 1] != ":"):
+                cpt_semi_colon_end = i - 1  # -1 pour garder 2 :: et non un seul :
+                break  # on a trouvé la chaine de :::: on peut quitter il n'y en a qu'une seule par adresse ipv6
+
+        new_ipv6 = ipv6[:cpt_semi_colon_start] + ipv6[cpt_semi_colon_end:]
+
+        return new_ipv6.removesuffix(":")
+
+
 
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@ Session HEADER @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     def UDP_Header(self, Data):
