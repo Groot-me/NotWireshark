82224e3044b85877d3d686b09e320611659b0601
grootme <nathan.jimenez13500@gmail.com>
2022-06-16 22:15:53 +0200

    Added ascii print on start

--- NotWireshark.py
+++ NotWireshark.py
@@ -10,7 +10,7 @@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 '''
 
-import socket, os
+import socket, os , argparse, random
 from PCAPFile import *
 from Trame import *
 
@@ -24,17 +24,27 @@ from Trame import *
 def main():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
     pcap,name_file = Create_pcapfile()
-
     cpt = 1
 
+    tab =[BBlue,BGreen,BCyan,BPurple,BYellow,BRed]
+    rand_int = random.randint(0,5)
+    print(f"""{tab[rand_int]}
+  _   _           _   __          __  _                      _                      _    
+ | \ | |         | |  \ \        / / (_)                    | |                    | |   
+ |  \| |   ___   | |_  \ \  /\  / /   _   _ __   ___   ___  | |__     __ _   _ __  | | __
+ | . ` |  / _ \  | __|  \ \/  \/ /   | | | '__| / _ \ / __| | '_ \   / _` | | '__| | |/ /
+ | |\  | | (_) | | |_    \  /\  /    | | | |   |  __/ \__ \ | | | | | (_| | | |    |   < 
+ |_| \_|  \___/   \__|    \/  \/     |_| |_|    \___| |___/ |_| |_|  \__,_| |_|    |_|\_\\                                                                                                                                                          
+     
+     {Reset}""")
+
     print("Press Ctrl+C to stop capturing... ")
-    time.sleep(1)
+    time.sleep(2)
 
     try:
         while True:
-            raw_data, addr = conn.recvfrom(65535)
+            raw_data, addr = conn.recvfrom(65635)
             pcap.write(raw_data)
-
             print(f"{BWhite}\n------------- Frame n° {cpt} ------------- {Reset}")
             Trame(raw_data.hex())
             cpt += 1
@@ -60,6 +70,10 @@ def Create_pcapfile():
         pcap = PCAPFile(f"/root/NotWireshark/notwireshark_{current_time}.pcap")
 
     return pcap, f"notwireshark_{current_time}.pcap"
+
+
+
+
 
 if __name__ == '__main__':
     main()
